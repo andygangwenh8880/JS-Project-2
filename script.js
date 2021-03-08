50cea930bd69804454b9a8b4cfb8fdcae4b345c3
Andy Huang <andygangwenh8880@hstat.org>
2021-03-08 16:26:10 +0000

    update repo

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 $.ajax({
-    url: 'https://api.publicapis.org/entries',
+    url: 'https://data.cityofnewyork.us/resource/ia2d-e54m.json',
     dataType: 'json',
     success: function(data) {
-        console.log(data);
+        document.write("<h1> Water Consumption in New York City (1979 - 2019)");
+        var year = Math.floor(prompt("Please input a year in between 1978 and 2020"));
+        for(i = 0; i < data.length; i++){
+            var Array = [];
+            Array.push(data[i].year + ", " + data[i].new_york_city_population + ", " + data[i].nyc_consumption_million_gallons_per_day);
+            if(year == data[i].year){
+                document.write("<br>");
+                document.write("<br>");
+                document.write("Year: " + data[i].year);
+                document.write("<br>");
+                document.write("<br>");
+                document.write("Population in New York City: " + Math.floor(data[i].new_york_city_population).toLocaleString() + " people");
+                document.write("<br>");
+                document.write("<br>");
+                document.write(" Water Consumption: " + Number(data[i].nyc_consumption_million_gallons_per_day).toLocaleString() + " gallons of water per person per day");
+            }
+        } 
+        
     }
 });
