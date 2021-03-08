820332cdb8f873f703a3cee2dfff54982a829bf6
Gavin Lee <GavinL0588@hstat.org>
2021-03-08 16:41:19 +0000

    update repo

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ $.ajax({
     success: function(data) {
         document.write("<h1> Water Consumption in New York City (1979 - 2019)");
         var year = Math.floor(prompt("Please input a year in between 1978 and 2020"));
+           if(year <= 1978){
+                document.write("<br>There is no data for the year " + year +"!");
+            } else if(year >= 2020){
+                document.write("<br>There is no data for the year " + year +"!");
+            }
         for(i = 0; i < data.length; i++){
             var Array = [];
             Array.push(data[i].year + ", " + data[i].new_york_city_population + ", " + data[i].nyc_consumption_million_gallons_per_day);
@@ -18,7 +23,8 @@ $.ajax({
                 document.write("<br>");
                 document.write(" Water Consumption: " + Number(data[i].nyc_consumption_million_gallons_per_day).toLocaleString() + " gallons of water per person per day");
             }
-        } 
-        
+
+        }
+
     }
 });
