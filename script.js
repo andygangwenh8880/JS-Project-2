794c18789ded7bdd0d7d50cfe9e775449e252d47
Andy Huang <andygangwenh8880@hstat.org>
2021-03-08 16:45:18 +0000

    update repo

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ $.ajax({
     success: function(data) {
         document.write("<h1> Water Consumption in New York City (1979 - 2019)");
         var year = Math.floor(prompt("Please input a year in between 1978 and 2020"));
-            document.write("<br>");
            if(year <= 1978){
+               document.write("<br>");
                 document.write("<br>There is no data for the year " + year +"!");
             } else if(year >= 2020){
+                document.write("<br>");
                 document.write("<br>There is no data for the year " + year +"!");
             }
         for(i = 0; i < data.length; i++){
