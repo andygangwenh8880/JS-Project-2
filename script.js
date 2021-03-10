0ad32c1fd69f620c7a0775d3b867903bf6bb4632
Andy Huang <andygangwenh8880@hstat.org>
2021-03-10 17:44:27 +0000

    update repo

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ $.ajax({
     success: function(data) {
         document.write("<h1> Water Consumption in New York City (1979 - 2019)");
         var year = Math.floor(prompt("Please input a year in between 1978 and 2020"));
+        // Restricts user from entering a year with no data
            if(year <= 1978){
                document.write("<br>");
                 document.write("<br>There is no data for the year " + year +"!");
@@ -11,9 +12,14 @@ $.ajax({
                 document.write("<br>");
                 document.write("<br>There is no data for the year " + year +"!");
             }
+            function converter(gallons){
+                return gallons*3.78541;
+            }
+            // loops through API and store info in array
         for(i = 0; i < data.length; i++){
             var Array = [];
             Array.push(data[i].year + ", " + data[i].new_york_city_population + ", " + data[i].nyc_consumption_million_gallons_per_day);
+            // Find users desired year and provide information of water comsumption of that year
             if(year == data[i].year){
                 document.write("<br>");
                 document.write("<br>");
@@ -23,7 +29,7 @@ $.ajax({
                 document.write("Population in New York City: " + Math.floor(data[i].new_york_city_population).toLocaleString() + " people");
                 document.write("<br>");
                 document.write("<br>");
-                document.write(" Water Consumption: " + Number(data[i].nyc_consumption_million_gallons_per_day).toLocaleString() + " gallons of water  per day");
+                document.write(" Water Consumption: " + Number(data[i].nyc_consumption_million_gallons_per_day).toLocaleString() + " gallons of water per day / " + converter(data[i].nyc_consumption_million_gallons_per_day) + " Liters per year");
             }
 
         }
